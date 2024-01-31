6bf6eab25044a6d1694a8643a5563f83c712bdc7
gregoryerrl <gregoryerrl@gmail.com>
2024-01-31 17:30:29 +0800

    putangina niyong lahat

--- CreatorsCove2/main.js
+++ CreatorsCove2/main.js
@@ -1,14 +1,18 @@
 var myIndex = 0;
-        carousel();
+carousel();
 
-        function carousel(){
-            var i;
-            var x = document.getElementsByClassName("slides");
-            for (i = 0; i < x.length; i++){
-                x[i].style.display = "none";
-            }
-            myIndex++;
-            if (myIndex > x.length) {myIndex = 1}
-            x[myIndex-1].style.display = "block";
-            setTimeout(carousel, 4000); //Image will change every 4 secs.
-        }
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 4000); //Image will change every 4 secs.
+}
+
+//example
